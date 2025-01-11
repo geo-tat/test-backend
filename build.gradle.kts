8e0a7215f6b6c295a2113292092bbe9fe81fbdac
geo-tat <nosleep2night@yandex.ru>
2025-01-12 00:31:07 +0400

    add docker-compose.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,8 @@ plugins {
 group = "mobi.sevenwinds"
 version = "0.0.1-SNAPSHOT"
 
-application {
-    mainClass.set("mobi.sevenwinds.ApplicationKt")
-    mainClassName = "mobi.sevenwinds.ApplicationKt"
-}
+application.mainClass.set("mobi.sevenwinds.ApplicationKt")
+application.applicationName = "mobi.sevenwinds.ApplicationKt"
 
 repositories {
     mavenCentral()
